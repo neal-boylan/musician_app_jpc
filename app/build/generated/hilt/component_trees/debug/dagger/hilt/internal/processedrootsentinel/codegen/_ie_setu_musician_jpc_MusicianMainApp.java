532de16a196f39c065d0b664078eb213f6e7f450
neal-boylan <nealboylan@gmail.com>
2024-12-15 11:12:08 +0000

    Trying new gitignore file

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_ie_setu_musician_jpc_MusicianMainApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "ie.setu.musician_jpc.MusicianMainApp"
-)
-public final class _ie_setu_musician_jpc_MusicianMainApp {
-}
